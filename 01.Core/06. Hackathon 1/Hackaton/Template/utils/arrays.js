00290d7696a3c2a70fcbc7e7ee78da4bdfc4d38c
SDonev <donevmail@gmail.com>
2023-04-19 08:25:32 +0300

    added task IndexOf from arrays

--- 01.Core/06. Hackathon 1/Hackaton/Template/utils/arrays.js
+++ 01.Core/06. Hackathon 1/Hackaton/Template/utils/arrays.js
@@ -27,8 +27,21 @@ const join = (array, separator = ',') => {
   // TODO
 };
 
+/**
+ * find the index of an given element in array
+ *
+ * @param {array}array The array to search in..
+ * @param {element}element The element to search for.
+ * @returns {number}The found index or -1.
+ */
 const indexOf = (array, element) => {
   // TODO
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 // hard
